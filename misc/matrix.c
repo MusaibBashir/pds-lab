ff6933a67b1f667d823a3e4ebf3a7123dc3a5d5b
Musaib Bin Bashir <musaibbashir02@gmail.com>
2024-09-02 19:08:19 +0530

    Decreasing matrix

--- misc/matrix.c
+++ misc/matrix.c
@@ -13,14 +13,14 @@ int main()
     printf("Enter n: ");
     scanf("%d", &n);
     int len = 2 * n - 1;
-    int min1, min2, min;
+    int minh, minv, min;
     for (int i = 1; i <= len; i++)  //Rows
     {
       for (int j = 1; j <= len; j++) //Columns
       { 
-        min1 = i <= len - i ? i -1 : len - i; //Min dist from horizontal edges
-        min2 = j <= len - j ? j - 1: len - j; // Min dist from vertical edges
-        min = min1 <= min2 ? min1 : min2; //Min dist of the above two
+        minh = i <= len - i ? i -1 : len - i; //Min dist from horizontal edges
+        minv = j <= len - j ? j - 1: len - j; // Min dist from vertical edges
+        min = minh <= minv ? minh : minv; //Min dist of the above two
         printf("%d ",n - min); //n-min came from observation of pattern
       }
       printf("\n");
